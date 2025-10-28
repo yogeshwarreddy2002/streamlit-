00266e56ab9a1623a13b6b00e106e281feb705f3
yogeshwarreddy2002 <yogeshwarreddy416@gmail.com>
2025-10-28 22:12:59 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.header("ms dhoni")
+st.write("Mahendra Singh Dhoni, popularly known as MS Dhoni, is one of the greatest cricketers in the world. He was born on July 7, 1981, in Ranchi, Jharkhand. Dhoni is famous for his calm nature, sharp decision-making, and excellent leadership skills. Under his captaincy, India won major trophies like the ICC T20 World Cup in 2007, the ICC Cricket World Cup in 2011, and the ICC Champions Trophy in 2013. He is also known as “Captain Cool” for his ability to stay calm under pressure. Apart from international cricket, Dhoni has led the Chennai Super Kings (CSK) to several IPL titles. His journey from a small-town boy to one of the most successful captains in cricket history is truly inspiring.")
+st.subheader("Achievements of MS Dhoni")
+st.markdown("""
+- 🏆 Won **ICC T20 World Cup (2007)**
+- 🥇 Won **ICC Cricket World Cup (2011)**
+- 🏅 Won **ICC Champions Trophy (2013)**
+- 💛 Led **Chennai Super Kings (CSK)** to multiple IPL titles
+- ⚡ Known as **Captain Cool** for his calm attitude under pressure
+""")
+st.markdown("""[Learn more about MS Dhoni](https://www.britannica.com/biography/MS-Dhoni)
+            |[wekipedia]("https://en.wikipedia.org/wiki/MS_Dhoni)""")
